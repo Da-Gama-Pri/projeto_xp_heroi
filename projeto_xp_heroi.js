2df1f06c1b5163ff7504b5e938db3d01113903f2
Da-Gama-Pri <pgama22@hotmail.com>
2023-12-03 19:26:30 -0300

    meu segundo

--- projeto_xp_heroi.js
+++ projeto_xp_heroi.js
@@ -1,2 +1,13 @@
-let nome_heroi ("Genoveva")
-let XP
+let nome_heroi = ("Genoveva")
+let XP = 900
+
+switch (XP) {
+    case XP < 1000:
+        console.log ("Nivel Ferro")
+        break;
+
+    default:
+        break;
+} (XP)
+
+console.log ("Seu jogador " + nome_heroi + " é nivel " + XP)
