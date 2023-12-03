017e94350031696b798a160fc1d5eec6a49b5fec
Da-Gama-Pri <pgama22@hotmail.com>
2023-12-03 19:38:32 -0300

    meu terceiro

--- projeto_xp_heroi.js
+++ projeto_xp_heroi.js
@@ -1,13 +1,30 @@
-let nome_heroi = ("Genoveva")
-let XP = 900
+let nome = "Genoveva"
+let XP = 3333
 
-switch (XP) {
-    case XP < 1000:
-        console.log ("Nivel Ferro")
-        break;
+    if(XP <= 1000){
+        XP = "Ferro"
+    }
+        else if(XP > 1000 && XP <= 2000){
+        XP = "Bronze"
+    }
+        else if(XP > 2000 && XP <= 5000){
+        XP = "Prata"
+    }
+        else if(XP > 5000 && XP <= 7000){
+        XP = "Ouro"
+    }
+        else if(XP > 7000 && XP <= 8000){
+        XP = "Platina"
+    }
+        else if(XP > 8000 && XP <= 9000){
+        XP = "Ascendente"
+    }
+        else if(XP > 9000 && XP <= 10000){
+        XP = "Imortal"
+    }
+        else if(XP > 10000){
+        XP = "Radiante"
+    }
+        console.log ("O Héroi de nome " + nome + ", está no nível " + XP)
 
-    default:
-        break;
-} (XP)
-
-console.log ("Seu jogador " + nome_heroi + " é nivel " + XP)
+console.log ("Seu jogador " + nome + " é nivel " + XP)
